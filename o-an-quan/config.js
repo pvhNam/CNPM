c4e0f39bb394392a93f0c36972b30709ea3579cb
pvhNam <phamvanhoain@gmail.com>
2026-05-25 12:33:43 +0700

    tạo project mới để làm bài tiếp theo

--- o-an-quan/config.js
+++
@@ -1,8 +0,0 @@
-/* ===================================
-   Server Config
-   Đổi SERVER_URL thành URL Render của bạn khi deploy
-   =================================== */
-
-// Khi chạy local: dùng '' (empty) để dùng relative URL
-// Khi deploy: đổi thành URL Render, ví dụ: 'https://o-an-quan-server.onrender.com'
-const SERVER_URL = '';
